775f9f4785015760b352257188fafff6c6e55f40
Piyush Goel <piyush.goel996@gmail.com>
2020-04-01 01:14:29 +0530

    double check locking

--- singleton/src/main/java/com/practice/example/LazySingleton.java
+++ singleton/src/main/java/com/practice/example/LazySingleton.java
@@ -1,18 +1,22 @@
 package com.practice.example;
 
 public class LazySingleton {
-
+	
 	private LazySingleton() {
 		System.out.println("LazySingleton is initializing");
 	}
-
+	
 	private static LazySingleton lazyInstance;
-
-	//synchronized method will be having performance implications
-	public static synchronized LazySingleton getInstance() {
-		if (lazyInstance == null) {
-			lazyInstance = new LazySingleton();
-		}
+	
+	// double check locking but its out-dated, its just a kind of textbook implementation.
+	public static LazySingleton getInstance() {
+		if(lazyInstance == null) {
+			synchronized(LazySingleton.class) {
+				if(lazyInstance == null) {
+					lazyInstance = new LazySingleton();
+				}
+			}
+		} 
 		return lazyInstance;
 	}
 }
