d85821a7a8cdf59d52b69539bcc1176e6456a7bc
Piyush Goel <piyush.goel996@gmail.com>
2020-04-01 01:08:03 +0530

    synchronized method for thread safety

--- singleton/src/main/java/com/practice/example/LazySingleton.java
+++ singleton/src/main/java/com/practice/example/LazySingleton.java
@@ -1,17 +1,18 @@
 package com.practice.example;
 
 public class LazySingleton {
-	
+
 	private LazySingleton() {
 		System.out.println("LazySingleton is initializing");
 	}
-	
+
 	private static LazySingleton lazyInstance;
-	
-	public static LazySingleton getInstance() {
-		if(lazyInstance == null) {
+
+	//synchronized method will be having performance implications
+	public static synchronized LazySingleton getInstance() {
+		if (lazyInstance == null) {
 			lazyInstance = new LazySingleton();
-		} 
+		}
 		return lazyInstance;
 	}
 }
